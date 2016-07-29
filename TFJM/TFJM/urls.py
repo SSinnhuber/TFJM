1b63b0f05a14ff35bdee30e2d30587fa4a2e19c7
irish <vzs@hotmail.fr>
2016-07-29 13:37:14 +0200

    modification des paramètres MEDIA

--- TFJM/TFJM/urls.py
+++ TFJM/TFJM/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^', include ('infos.urls')),
 ]
 
-if getattr(settings, 'DEBUG', False) or getattr(settings, 'DEBUG_MEDIA', False):
+"""if getattr(settings, 'DEBUG', False) or getattr(settings, 'DEBUG_MEDIA', False):
     media_url = getattr(settings, 'MEDIA_URL', '/media/').lstrip('/')
     urlpatterns = [
         url(r'^%s(?P<path>.*)$' % (media_url,), 'django.views.static.serve', {
@@ -31,3 +31,4 @@ if getattr(settings, 'DEBUG', False) or getattr(settings, 'DEBUG_MEDIA', False):
         }),
     ] + urlpatterns
 
+"""
