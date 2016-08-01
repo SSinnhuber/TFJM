d926a6e09198e4a82317a56bd0cc71be9f97614c
irish <vzs@hotmail.fr>
2016-08-01 15:08:03 +0200

    ajout d'un système de gestion d'utilisateur

--- TFJM/profils/models.py
+++ TFJM/profils/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profil (models.Model):
+	user = models.OneToOneField (User)
+	bio = models.TextField (blank=True)
+	
+	def __str__ (self) :
+		return self.user.username
--- TFJM/profils/views.py
+++ TFJM/profils/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+from forms import ConnexionForm
+from infos import views
+
+def connexion(request) :
+	error = False
+	print ("")
+	if request.method == 'POST':	
+		form = ConnexionForm(request.POST)
+		if form.is_valid():
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password']
+			user = authenticate(username=username, password=password) 
+			if user is not None:
+				if user.is_active:
+					login(request, user)
+					return redirect (views.accueil)
+				else:
+					error = True
+			else:
+				error = True
+	else:
+		form = ConnexionForm()
+	return render(request, 'profils/connexion.html', locals())
+
+
+
+def deconnexion(request):
+    logout(request)
+    return redirect(views.accueil)
